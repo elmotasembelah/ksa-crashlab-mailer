0cd460f959a39b82271795b38d005fad62fdfb65
elmoatasmbelah <moatasm-belah@spatiumsoftware.com>
2024-04-29 11:19:27 +0300

    remove cors config

--- index.js
+++ index.js
@@ -11,22 +11,31 @@ app.use(express.json());
 
 // security
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000,
-    max: 1000,
+    max: 20,
   })
 );
 
+// const corsConfig = {
+//   origin: "https://www.ksacrashlab.com",
+// };
+
+// const corsConfig = {
+//   origin: "https://radiant-peony-76a502.netlify.app",
+// };
+
+// app.use(cors(corsConfig));
+
+app.use(cors());
 app.get("/", (req, res) => {
-  console.log(req.body);
   res.send("connected");
 });
 
-app.post("/sendEmail", sendEmailUsingGmail)
+app.post("/sendEmail", sendEmailUsingGmail);
 
 app.use((req, res) => res.status(404).send("Route does not exist"));
 
